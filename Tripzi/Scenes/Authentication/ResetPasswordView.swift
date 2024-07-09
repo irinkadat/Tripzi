9bc843fadbbcb057576b22171edf3a95ae0e6c64
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-07-09 11:15:47 +0400

    Add Reset Password Functionality using firebase

--- Tripzi/Scenes/Authentication/ResetPasswordView.swift
+++ Tripzi/Scenes/Authentication/ResetPasswordView.swift
@@ -5,4 +5,27 @@
 //  Created by Irinka Datoshvili on 27.06.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ResetPasswordView: View {
+    @Binding var email: String
+    @Environment(\.presentationMode) var presentationMode
+    var action: (String) -> Void
+    
+    var body: some View {
+        VStack {
+            CustomTextField(placeholder: "Enter your email", text: $email)
+            
+            CustomButton(title: "Send Reset Link", backgroundColor: .green, textColor: .white, action: {
+                action(email)
+                presentationMode.wrappedValue.dismiss()
+            })
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ResetPasswordView(email: .constant(""), action: { _ in })
+        .environmentObject(AuthenticationViewModel())
+}
