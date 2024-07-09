6f9e4ddd94517460bd15672622fef81db79b1aa0
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-07-09 11:13:35 +0400

    Add Home, Profile, Favourites and profile pages

--- Tripzi/Scenes/Profile/ProfileVC.swift
+++ Tripzi/Scenes/Profile/ProfileVC.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileVC: UIViewController {
-
+    
+    private var authManager = AuthenticationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLogoutButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupLogoutButton() {
+        let logoutButton = UIButton(type: .system)
+        logoutButton.setTitle("Logout", for: .normal)
+        logoutButton.backgroundColor = .systemBlue
+        logoutButton.setTitleColor(.white, for: .normal)
+        logoutButton.layer.cornerRadius = 10
+        logoutButton.addTarget(self, action: #selector(handleLogout), for: .touchUpInside)
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        
+        NSLayoutConstraint.activate([
+            logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logoutButton.widthAnchor.constraint(equalToConstant: 200),
+            logoutButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
-    */
-
+    
+    @objc private func handleLogout() {
+        authManager.signOut()
+        presentLoginScreen()
+        
+    }
 }
+
+private func presentLoginScreen() {
+    if let sceneDelegate = UIApplication.shared.connectedScenes
+        .first(where: { $0.activationState == .foregroundActive })?.delegate as? SceneDelegate {
+        sceneDelegate.resetToLoginScreen()
+    }
+}
+
+private func showErrorMessage(_ message: String) {
+    let alert = UIAlertController(title: "Logout Failed", message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default))
+}
+
+
