5d4fc250cc341c736d2b66d0bd91342cbaf2a979
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-09-11 12:35:28 +0400

    Add functionality to show all locations on home page

---
+++ Tripzi/Scenes/Shared/Custom Components/CustomViews/CustomPageControl.swift
@@ -0,0 +1,8 @@
+//
+//  CustomPageControl.swift
+//  Tripzi
+//
+//  Created by Irinka Datoshvili on 06.09.24.
+//
+
+import Foundation
---
+++ Tripzi/Utilities/CoordinateUtils.swift
@@ -0,0 +1,8 @@
+//
+//  CoordinateUtils.swift
+//  Tripzi
+//
+//  Created by Irinka Datoshvili on 09.09.24.
+//
+
+import Foundation
